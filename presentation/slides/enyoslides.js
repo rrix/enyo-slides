a46f959c10aab08fb1037180349e6bf41d92b25e
Ryan Rix <ry@n.rix.si>
2012-08-02 03:36:28 -0700

    Enyoslides slide

---
+++ presentation/slides/enyoslides.js
@@ -0,0 +1,71 @@
+enyo.kind({
+  kind: 'slidedeck.Slide',
+  name: 'enyoslides',
+
+  components: [
+    {
+      kind: 'onyx.Toolbar',
+      content: 'EnyoSlides - EnyoJS as a Web Toolkit',
+      classes: 'top-toolbar'
+    },
+    {
+      kind: 'enyo.Image',
+      classes: 'right-photo',
+      src: '$presentation/assets/images/enyoslides.png'
+    },
+    {
+      tag: 'ul',
+      components: [
+        {
+          tag: 'li',
+          content: 'Developed to show off Enyo'
+        },
+        {
+          tag: 'li',
+          components: [
+            {
+              content: 'Typical pure JS application stack'
+            },
+            {
+              tag: 'ul',
+              components: [
+                {
+                  tag: 'li',
+                  content: 'Node.JS, ExpressJS, MongoDB, Socket.io, EnyoJS (Duh!)'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          tag: 'li',
+          components: [
+            {
+              content: 'Heavy focus on re-usability'
+            },
+            {
+              tag: 'ul',
+              components: [
+                {
+                  tag: 'li',
+                  content: 'Express application designed to dynamically pull slides'
+                },
+                {
+                  tag: 'li',
+                  content: 'Reuses code from community gallery'
+                },
+                {
+                  tag: 'li',
+                  content: 'As much code as possible is placed in $lib for eventual gallery inclusion'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          tag: ''
+        }
+      ]
+    }
+  ]
+});
