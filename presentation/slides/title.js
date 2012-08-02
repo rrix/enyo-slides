a847b3f546d831ac605f74bfeff2bd8c145f285c
Ryan Rix <ry@n.rix.si>
2012-08-02 04:39:49 -0700

    Make title page link to my twitter

--- presentation/slides/title.js
+++ presentation/slides/title.js
@@ -7,7 +7,16 @@ enyo.kind({
     {tag: "div", classes: "title-text", components: [
       {tag: "h1",  content: "EnyoJS Hackathon"},
       {tag: "h2",  content: "Ryan Rix"},
-      {tag: "h2",  content: "(@rrrrrrrix)"},
+      {
+        tag: "h2",
+        components: [
+          {
+            tag: 'a',
+            attributes: {href: 'http://twitter.com/rrrrrrrix/', target: '_blank'},
+            content: "(@rrrrrrrix)"
+          }
+        ]
+      },
       {tag: "h2",  content: "http://rrix-hackathon.nodester.com"},
       {tag: "h2",  content: "4 August 2012"}
     ]}
